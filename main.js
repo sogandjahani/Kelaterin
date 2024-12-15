5ca832d809b81cf2284ff76712525711c4e58a56
sogandjahani <sogandjahani1380@yahoo.com>
2024-12-15 11:08:32 +0330

    Kelaterin 1.1.3

--- main.js
+++ main.js
@@ -1,12 +1,2 @@
-let themeToggler = document.getElementById('theme-toggler');
-
-themeToggler.onclick = () => {
-  themeToggler.classList.toggle('fa-sun');
-  themeToggler.classList.toggle('fa-moon');
-
-  if (themeToggler.classList.contains('fa-sun')) {
-    document.documentElement.classList.add('dark-mode');
-  } else {
-    document.documentElement.classList.remove('dark-mode');
-  }
-};
+import 'swiper/swiper-bundle.min.css'; // برای CSS
+import Swiper from 'swiper'; // برای JS
